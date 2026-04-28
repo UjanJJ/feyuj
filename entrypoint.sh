e0f9e5fdf88a4ed848d3471e1a365b932063fc34
UjanJJ <flf28396@gmail.com>
2026-04-28 21:58:31 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Генерация параметров
 UUID=${UUID:-$(cat /proc/sys/kernel/random/uuid)}
 SHORT_ID=${SHORT_ID:-$(openssl rand -hex 8)}
 
-# Генерация ключей Reality
 KEYS=$(xray x25519)
 PRIVATE_KEY=$(echo "$KEYS" | grep "Private key" | awk '{print $3}')
 PUBLIC_KEY=$(echo "$KEYS" | grep "Public key" | awk '{print $3}')
 
-# Настройки SNI
 DEST=${DEST:-"ads.x5.ru:443"}
 SERVER_NAME=${SERVER_NAME:-"ads.x5.ru"}
 
@@ -23,9 +20,16 @@ echo "SNI        : ${SERVER_NAME}"
 echo "Порт       : ${PORT}"
 echo "========================================"
 
-# Создаём конфиг из шаблона
-envsubst < /etc/xray/config.json.template > /etc/xray/config.json
+# Копируем шаблон и заменяем значения через sed (более надёжно)
+cp /etc/xray/config.json.template /etc/xray/config.json
 
-echo "Конфиг создан успешно. Запускаю Xray..."
+sed -i "s|\${UUID}|${UUID}|g" /etc/xray/config.json
+sed -i "s|\${SHORT_ID}|${SHORT_ID}|g" /etc/xray/config.json
+sed -i "s|\${PRIVATE_KEY}|${PRIVATE_KEY}|g" /etc/xray/config.json
+sed -i "s|\${DEST}|${DEST}|g" /etc/xray/config.json
+sed -i "s|\${SERVER_NAME}|${SERVER_NAME}|g" /etc/xray/config.json
+sed -i "s|\${PORT}|${PORT}|g" /etc/xray/config.json
+
+echo "Конфиг успешно создан с помощью sed. Запускаю Xray..."
 
 exec xray run -c /etc/xray/config.json
